f3b5d1d946cc7399fb36d135aa11b3c3efb57045
noah-elston <90793666+noah-elston@users.noreply.github.com>
2023-09-25 21:15:48 -0400

    updoot

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,11 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "I like trains";
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to change the header text
+    function changeHeader() {
+        const header = document.getElementById("myHeader");
+        header.textContent = "Header changed after 10 seconds";
+    }
+
+    // Set a timeout to call the function after 10 seconds
+    setTimeout(changeHeader, 10000); // 10000 milliseconds = 10 seconds
+});
